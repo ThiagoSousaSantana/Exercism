51a7189d218a25b61e1444f4a8684efc790bb2e1
Thiago Sousa Santana <thiago91098@gmail.com>
2019-05-17 17:28:49 -0300

    Fixing matrix

--- java/matrix/src/main/java/Matrix.java
+++ java/matrix/src/main/java/Matrix.java
@@ -1,47 +1,22 @@
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 class Matrix {
 
     private int[][] matrix;
-    private List<String> columns;
-    private List<String> rows;
 
     Matrix(String matrixAsString) {
-        stringToMatrix(matrixAsString);
+        this.matrix = Arrays.stream(matrixAsString.split("\n"))
+                .map(line -> Arrays.stream(line.split(" "))
+                    .mapToInt(n -> Integer.parseInt(n))
+                    .toArray())
+                .toArray(int[][]::new);
     }
 
     int[] getRow(int rowNumber) {
-        return this.matrix[rowNumber];
+        return this.matrix[rowNumber].clone();
     }
 
     int[] getColumn(int columnNumber) {
-        var column = new int[rows.size()];
-
-        for (int i = 0; i < rows.size(); i++)
-            column[i] = Integer.parseInt(rows.get(i).split(" ")[columnNumber]);
-
-       return column;
-    }
-
-    int getRowsCount() {
-        return rows.size();
-    }
-
-    int getColumnsCount() {
-        return columns.size();
-    }
-
-    private void stringToMatrix(String string){
-        rows = Arrays.asList(string.split("\n"));
-        this.matrix = new int[rows.size()][rows.get(0).split(" ").length];
-
-        for(var i = 0; i <= rows.size() - 1; i++){
-            columns = Arrays.asList(rows.get(i).split(" "));
-
-            for (int i1 = 0; i1 <= columns.size() - 1; i1++)
-                this.matrix[i][i1] = Integer.parseInt(columns.get(i1));
-        }
+       return null;
     }
 }
